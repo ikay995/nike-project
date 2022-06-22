7f69a84644e8809e89eb1f1090b994a0e5244fdb
ikay995 <kalukingsley995@gmail.com>
2022-06-22 11:14:19 -0700

    commit

---
+++ src/Context.js
@@ -0,0 +1,61 @@
+import React, {useState, useEffect} from "react"
+import Image from "./utils/images.json"
+
+const Context = React.createContext()
+
+function ContextProvider({children}) {
+    const [allPhotos, setAllPhotos] = useState([])
+    const [cartItems, setCartItems] = useState([])
+    
+    
+    useEffect(()=> {
+        fetch('https://ikay995.github.io/nike-project/images.json', {
+            headers : { 
+              'Content-Type': 'application/json',
+              'Accept': 'application/json'
+             }
+      
+          })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        
+  },[])
+    
+    function toggleFavorite(id) {
+        const updatedArr = allPhotos.map(photo => {
+            if(photo.id === id) {
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        
+        setAllPhotos(updatedArr)
+    }
+    
+    function addToCart(newItem) {
+        setCartItems(prevItems => [...prevItems, newItem])
+    }
+    
+    function removeFromCart(id) {
+        setCartItems(prevItems => prevItems.filter(item => item.id !== id))
+    }
+    
+    function emptyCart() {
+        setCartItems([])
+    }
+    
+    return (
+        <Context.Provider value={{
+            allPhotos, 
+            toggleFavorite, 
+            cartItems, 
+            addToCart, 
+            removeFromCart, 
+            emptyCart
+        }}>
+            {children}
+        </Context.Provider>
+    )
+}
+
+export {ContextProvider, Context}
